adcb598519b17ab519bf4847bcc7d60fa57fc345
RCAbney <ryan@rcabney.com>
2021-08-28 00:49:42 -0400

    refactor title tags to accept frontmatter title add favicon

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,34 @@
-import Layout from '../components/layout'
-import PortfolioList from '../components/portfolioList'
+import Layout from "../components/layout";
+import Head from "next/head";
+import PortfolioList from "../components/portfolioList";
 
-import { getSortedPostsData } from '../lib/posts'
+import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData,
+    },
+  };
 }
 
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
-    <Layout>
-
-      <div className="intro">
-        <h1 className="intro-title">
-          Ryan Abney,
-          <br />
-          Front-End Developer and Designer
-        </h1>
-      </div>
-      <PortfolioList works={allPostsData} />
-
-    </Layout>
-  )
+    <>
+      <Head>
+        <title>Ryan Abney | Developer</title>
+      </Head>
+      <Layout>
+        <div className="intro">
+          <h1 className="intro-title">
+            Ryan Abney,
+            <br />
+            Front-End Developer and Designer
+          </h1>
+        </div>
+        <PortfolioList works={allPostsData} />
+      </Layout>
+    </>
+  );
 }
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,23 +1,29 @@
-import Layout from '../components/layout'
-import PortfolioList from '../components/portfolioList'
-import { getSortedPostsData } from '../lib/posts'
+import Layout from "../components/layout";
+import Head from "next/head";
+import PortfolioList from "../components/portfolioList";
+import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
-    const allPostsData = getSortedPostsData()
-    return {
-        props: {
-            allPostsData
-        }
-    }
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
 
 export default function PortfolioPage({ allPostsData }) {
-    return (
-        <Layout>
-            <div className="intro">
-                <h1>Portfolio</h1>
-            </div>
-            <PortfolioList works={allPostsData} />
-        </Layout>
-    )
+  return (
+    <>
+      <Head>
+        <title>Ryan Abney | Developer - Portfolio</title>
+      </Head>
+      <Layout>
+        <div className="intro">
+          <h1>Portfolio</h1>
+        </div>
+        <PortfolioList works={allPostsData} />
+      </Layout>
+    </>
+  );
 }
