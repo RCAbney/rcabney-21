6e51d657f2dcb5103d242601b0eca87da4f73aa3
RCAbney <ryan@rcabney.com>
2021-08-28 15:15:42 -0400

    add analytics snippet to head

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.scss'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALTICS, {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
