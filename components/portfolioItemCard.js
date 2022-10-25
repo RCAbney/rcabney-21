958180288443e1fc8ffff228035cb9a9a3ef7007
RCAbney <ryan@rcabney.com>
2022-10-25 19:56:43 -0400

    add plaiceholder

--- components/portfolioItemCard.js
+++ components/portfolioItemCard.js
@@ -1,23 +1,30 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import WorkTags from './workTags'
+import Link from "next/link";
+import Image from "next/image";
+import WorkTags from "./workTags";
 
 export default function PortfolioItemCard({ image, title, tags, slug }) {
-    return (
-        <div className="card">
-        <Link href={`/portfolio/${slug}`}>
-            <a>
-              <Image src={image} className="folio-image" alt={title} width={1000} height={1000} layout="intrinsic" />
-            </a>
-        </Link>
-        <div className="folio-details">
-          <h2 className="folio-headline">
-            <Link href={`/portfolio/${slug}`}>
-              <a className="folio-link" >{title}</a>
-            </Link>
-          </h2>
-        </div>
-        {tags && <WorkTags tags={tags} />}
+  return (
+    <div className="card">
+      <Link href={`/portfolio/${slug}`}>
+        <a>
+          <Image
+            src={image}
+            className="folio-image"
+            alt={title}
+            width={1000}
+            height={1000}
+            layout="intrinsic"
+          />
+        </a>
+      </Link>
+      <div className="folio-details">
+        <h2 className="folio-headline">
+          <Link href={`/portfolio/${slug}`}>
+            <a className="folio-link">{title}</a>
+          </Link>
+        </h2>
       </div>
-    )
+      {tags && <WorkTags tags={tags} />}
+    </div>
+  );
 }
